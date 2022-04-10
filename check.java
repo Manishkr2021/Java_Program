abf34c4309066167df1fc9b1f0658c66839a60b0
Manish <manishnpu2018@gmail.com>
2022-04-10 22:23:32 +0530

    first commit

---
+++ check.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class check{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no from 1 to 5 : ");
+        int n=sc.nextInt();
+        switch(n){
+            case 1:
+            System.out.println("1");
+            break;
+            case 2:
+            System.out.println("2");
+            break;
+            case 3:
+            System.out.println("3");
+            break;
+            case 4:
+            System.out.println("4");
+            break;
+            case 5:
+            System.out.println("5");
+            break;
+            default:
+            break;
+        }
+    }
+}
